bfe1d3a69976d2655643185d944f21ec09c5d563
Emilio Esteban <delhierroemilio@gmail.com>
2025-07-15 20:54:06 -0500

    Hero realizado

---
+++ src/sections/NavBar.jsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <img src="/images/nav-logo.svg" alt="gta 6 logo" className="scale-90" />
+      <img src="/images/menu.svg" alt="menu icon" className="w-10" />
+    </nav>
+  );
+};
+
+export default NavBar;
